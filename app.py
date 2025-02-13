1cb9312b4e3a207ab2c6737b79f5e3d501db5c6e
Rivalz <114005036+Rivalz58@users.noreply.github.com>
2025-02-13 11:53:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def processed_image(filename):
     return send_from_directory(app.config["PROCESSED_FOLDER"], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
